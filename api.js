f959dceebe831960f721e4900f361c05e88bcef7
John Beech <john.beech@bbc.co.uk>
2016-01-05 09:48:52 +0000

    Created api.js

---
+++ api.js
@@ -0,0 +1,9 @@
+var clean = require('clean');
+var fetch = require('fetch');
+var find = require('find');
+var read = require('read');
+var write = require('write');
+
+module.exports = {
+    clean, fetch, find, read, write
+};
