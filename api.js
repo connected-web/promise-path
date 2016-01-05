6ea90c91d7f383f4552023cdca2213b6d7f09097
John Beech <john.beech@bbc.co.uk>
2016-01-05 12:04:38 +0000

    Implemented tests for each method

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
-var clean = require('clean');
-var fetch = require('fetch');
-var find = require('find');
-var read = require('read');
-var write = require('write');
+var clean = require('./lib/path/clean');
+var fetch = require('./lib/path/fetch');
+var find = require('./lib/path/find');
+var read = require('./lib/path/read');
+var write = require('./lib/path/write');
 
 module.exports = {
     clean, fetch, find, read, write
--- test/api-spec.js
+++ test/api-spec.js
@@ -1,3 +1,4 @@
+const api = require('../api');
 const expect = require('chai').expect;
 
 var pending = function(done) {
@@ -6,32 +7,83 @@ var pending = function(done) {
 
 describe('API', function() {
     describe('clean', function() {
+        var someFile = 'temp/some-file.txt';
+        beforeEach(function(done) {
+            api.write(someFile, 'Some Data').then(done).catch(done);
+        });
+
         it('should remove all files and folders in a directory', function(done) {
-            pending(done);
+            api.clean('temp')
+                .then(() => api.read(someFile))
+                .then(function() {
+                    done('Unexpected file still exists');
+                }).catch(function(ex) {
+                    done();
+                });
         });
     });
 
     describe('fetch', function() {
+        var remoteFile = 'https://raw.githubusercontent.com/connected-web/remote-test/master/info.json';
         it('should fetch a remote file', function(done) {
-            pending(done);
+            api.fetch(remoteFile)
+                .then(JSON.parse)
+                .then(function(data) {
+                    expect(data.message).to.equal(`If you're reading this JSON file you've successfully accessed the remote test`);
+                })
+                .then(done)
+                .catch(done);
         });
     });
 
     describe('find', function() {
+
+        function stripPath(path, file) {
+            return file.replace(path, '');
+        }
+
         it('should return a list of files based on a glob pattern', function(done) {
-            pending(done);
+            var path = 'lib/path/';
+            api.find(path + '*.js')
+                .then((files) => files.map((file) => file.replace(path, '')))
+                .then(function(files) {
+                    expect(files).to.deep.equal([
+                        'clean.js',
+                        'fetch.js',
+                        'find.js',
+                        'read.js',
+                        'write.js'
+                    ]);
+                })
+                .then(done)
+                .catch(done);
         });
     });
 
     describe('read', function() {
         it('should read the contents of a file', function(done) {
-            pending(done);
+            api.read(__dirname + '/fixtures/sample.txt', 'utf8')
+                .then(function(contents) {
+                    expect(contents).to.equal(`Sample file with sample contents.`);
+                })
+                .then(done)
+                .catch(done);
         });
     });
 
     describe('write', function() {
         it('should write contents to a file', function(done) {
-            pending(done);
+            var file = 'temp/test.log';
+            var expectedContents = 'Sample log with sample text';
+            api.clean('temp')
+                .then(() => api.write(file, expectedContents))
+                .then(() => api.read(file, 'utf8'))
+                .then(function(actualContents) {
+                    expect(actualContents).to.equal(expectedContents);
+                })
+                .then(() => api.clean('temp'))
+                .then(done)
+                .catch(done);
         });
     });
 });
