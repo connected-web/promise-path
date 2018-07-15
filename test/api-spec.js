1aa7e7286513a3e2799bca051d98896802886382
John Beech <johnbeech.github@mkv25.net>
2018-07-15 21:47:40 +0100

    Rewrite api-spec using async await style commands

--- test/api-spec.js
+++ test/api-spec.js
@@ -11,154 +11,121 @@ describe('API', () => {
       api.write(someFile, 'Some Data').then(done).catch(done)
     })
 
-    it('should remove all files and folders in a directory', function (done) {
-      api.clean('temp')
-        .then(() => api.read(someFile))
-        .then(() => {
-          done('Unexpected file still exists')
-        }).catch(function (ex) {
-          done()
-        })
+    it('should remove all files and folders in a directory', async () => {
+      await api.clean('temp')
+      try {
+        await api.read(someFile)
+      } catch (ex) {
+        const actual = (ex || {}).code
+        return expect(actual).to.equal('ENOENT')
+      }
+      throw new Error('Unexpected file still exists')
     })
   })
 
   describe('fetch', () => {
     const remoteFile = 'https://raw.githubusercontent.com/connected-web/remote-test/master/info.json'
-    it('should fetch a remote file', function (done) {
-      api.fetch(remoteFile)
-        .then(JSON.parse)
-        .then(function (data) {
-          expect(data.message).to.equal(`If you're reading this JSON file you've successfully accessed the remote test`)
-        })
-        .then(done)
-        .catch(done)
+    it('should fetch a remote file', async () => {
+      const file = await api.fetch(remoteFile)
+      const data = JSON.parse(file)
+      expect(data.message).to.equal(`If you're reading this JSON file you've successfully accessed the remote test`)
     })
   })
 
   describe('find', () => {
-    it('should return a list of files based on a glob pattern', function (done) {
+    it('should return a list of files based on a glob pattern', async () => {
       const dir = 'lib/path/'
-      api.find(path.join(dir, '*.js'))
-        .then((files) => files.map((file) => file.replace(dir, '')))
-        .then(function (files) {
-          expect(files).to.deep.equal([
-            'clean.js',
-            'fetch.js',
-            'find.js',
-            'make.js',
-            'read.js',
-            'run.js',
-            'write.js'
-          ])
-        })
-        .then(done)
-        .catch(done)
+      const filepaths = await api.find(path.join(dir, '*.js'))
+      const actual = filepaths.map((file) => file.replace(dir, ''))
+      const expected = [
+        'clean.js',
+        'fetch.js',
+        'find.js',
+        'make.js',
+        'read.js',
+        'run.js',
+        'write.js'
+      ]
+      expect(actual).to.deep.equal(expected)
     })
   })
 
   describe('read', () => {
-    it('should read the contents of a file', function (done) {
-      api.read(path.join(__dirname, 'fixtures/sample.txt'), 'utf8')
-        .then(function (contents) {
-          expect(contents).to.equal(`Sample file with sample contents.`)
-        })
-        .then(done)
-        .catch(done)
+    it('should read the contents of a file', async () => {
+      const contents = await api.read(path.join(__dirname, 'fixtures/sample.txt'), 'utf8')
+      expect(contents).to.equal(`Sample file with sample contents.`)
     })
   })
 
   describe('write', () => {
-    it('should write contents to a file', function (done) {
+    it('should write contents to a file', async () => {
       const file = 'temp/test.log'
-      const expectedContents = 'Sample log with sample text'
+      const expected = 'Sample log with sample text'
+      await api.clean('temp')
+      await api.write(file, expected)
+      const actual = await api.read(file, 'utf8')
+      expect(actual).to.equal(expected)
+    })
+
+    afterEach(() => {
       api.clean('temp')
-        .then(() => api.write(file, expectedContents))
-        .then(() => api.read(file, 'utf8'))
-        .then(function (actualContents) {
-          expect(actualContents).to.equal(expectedContents)
-        })
-        .then(() => api.clean('temp'))
-        .then(done)
-        .catch(done)
     })
   })
 
   describe('make', () => {
-    it('should make a directory if it does not exist', function (done) {
+    it('should make a directory if it does not exist', async () => {
       const directory = 'temp/new-directory'
-      api.clean('temp')
-        .then(() => api.make(directory))
-        .then(function (result) {
-          const stats = fs.lstatSync(directory)
-          // Is it a directory?
-          expect(stats.isDirectory()).to.equal(true)
-        })
-        .then(() => api.clean('temp'))
-        .then(done)
-        .catch(done)
+      await api.clean('temp')
+      await api.make(directory)
+      const stats = fs.lstatSync(directory)
+      // Is it a directory?
+      expect(stats.isDirectory()).to.equal(true)
     })
 
-    it('should succeed if the directory already exists', function (done) {
+    it('should succeed if the directory already exists', async () => {
       const directory = 'temp/new-directory'
+      await api.clean('temp')
+      await api.make(directory)
+      await api.make(directory) // x2
+      const stats = fs.lstatSync(directory)
+      // Is it a directory?
+      expect(stats.isDirectory()).to.equal(true)
+    })
+
+    afterEach(() => {
       api.clean('temp')
-        .then(() => api.make(directory))
-        .then(() => api.make(directory))
-        .then(function (result) {
-          const stats = fs.lstatSync(directory)
-          // Is it a directory?
-          expect(stats.isDirectory()).to.equal(true)
-        })
-        .then(() => api.clean('temp'))
-        .then(done)
-        .catch(done)
     })
   })
 
   describe('run', () => {
-    it('should run the supplied command, and return the result', function (done) {
+    it('should run the supplied command, and return the result', async () => {
       const file = path.join(__dirname, 'fixtures/sample.txt')
-      let expected
-
-      api.read(file, 'utf8')
-        .then(function (body) {
-          expected = {
-            error: null,
-            exitCode: 0,
-            stdout: body,
-            stderr: ''
-          }
-        })
-        .then(() => api.run(`cat ${file}`))
-        .then(function (actual) {
-          expect(actual).to.deep.equal(expected)
-        })
-        .then(done)
-        .catch(done)
+      const body = await api.read(file, 'utf8')
+      const expected = {
+        error: null,
+        exitCode: 0,
+        stdout: body,
+        stderr: ''
+      }
+      const actual = await api.run(`cat ${file}`)
+      expect(actual).to.deep.equal(expected)
     })
 
-    it('should be able to run a command in a different working directory, and return the result', function (done) {
+    it('should be able to run a command in a different working directory, and return the result', async () => {
       const dir = 'test/fixtures'
       const file = 'sample.txt'
-      let expected
-
-      api.read(path.join(dir, file), 'utf8')
-        .then(function (body) {
-          expected = {
-            error: null,
-            exitCode: 0,
-            stdout: body,
-            stderr: ''
-          }
-        })
-        .then(() => api.run(`cat ${file}`, path.join(process.cwd(), dir)))
-        .then(function (actual) {
-          expect(actual).to.deep.equal(expected)
-        })
-        .then(done)
-        .catch(done)
+      const body = await api.read(path.join(dir, file), 'utf8')
+      const expected = {
+        error: null,
+        exitCode: 0,
+        stdout: body,
+        stderr: ''
+      }
+      const actual = await api.run(`cat ${file}`, path.join(process.cwd(), dir))
+      expect(actual).to.deep.equal(expected)
     })
 
-    it('should pass environment variables through to the child process', function (done) {
+    it('should pass environment variables through to the child process', async () => {
       const env = {
         x: 1 + Math.random(),
         y: 2 + Math.random(),
@@ -170,65 +137,43 @@ describe('API', () => {
         stdout: `${env.x} ${env.y} ${env.z}\n`,
         stderr: ''
       }
-
-      api.run('node env.js', path.join(__dirname, 'fixtures'), env)
-        .then(function (actual) {
-          expect(actual).to.deep.equal(expected)
-        })
-        .then(done)
-        .catch(done)
+      const actual = await api.run('node env.js', path.join(__dirname, 'fixtures'), env)
+      expect(actual).to.deep.equal(expected)
     })
 
-    it('should run a command with quoted value containing spaces', function (done) {
+    it('should run a command with quoted value containing spaces', async () => {
       const expected = {
         error: null,
         exitCode: 1,
         stdout: '',
         stderr: "cat: '\"the file name\"': No such file or directory\n"
       }
-
-      api.run(`cat "the file name"`)
-        .then(function (actual) {
-          expect(actual).to.deep.equal(expected)
-        })
-        .then(done)
-        .catch(done)
+      const actual = await api.run(`cat "the file name"`)
+      expect(actual).to.deep.equal(expected)
     })
 
-    it('should run a command with quoted values', function (done) {
+    it('should run a command with quoted values', async () => {
       const expected = {
         error: null,
         exitCode: 1,
         stdout: '',
         stderr: "cat: '\"nospacequote\"': No such file or directory\n"
       }
-
-      api.run(`cat "nospacequote"`)
-        .then(function (actual) {
-          expect(actual).to.deep.equal(expected)
-        })
-        .then(done)
-        .catch(done)
+      const actual = await api.run(`cat "nospacequote"`)
+      expect(actual).to.deep.equal(expected)
     })
 
-    it('should run a complex chain of asychronous commands, and return the result', function (done) {
-      this.timeout(15000)
-
+    it('should run a complex chain of asychronous commands, and return the result', async () => {
       const expected = {
         error: null,
         exitCode: 0,
         stdout: '',
         stderr: ''
       }
-
-      api.run(`touch hello.txt`)
-        .then(() => api.run(`cat hello.txt`))
-        .then(() => api.run(`rm hello.txt`))
-        .then(function (actual) {
-          expect(actual).to.deep.equal(expected)
-        })
-        .then(done)
-        .catch(done)
-    })
+      await api.run(`touch hello.txt`)
+      await api.run(`cat hello.txt`)
+      const actual = await api.run(`rm hello.txt`)
+      expect(actual).to.deep.equal(expected)
+    }).timeout(1500)
   })
 })
